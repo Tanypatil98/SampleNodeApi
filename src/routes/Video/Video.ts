2829cae86aaf0195e1c1ffafc172de26609c51fc
Tanay Patil <tanaypatil36@gmail.com>
2022-05-09 19:33:05 +0530

    minor

--- src/routes/Video/Video.ts
+++ src/routes/Video/Video.ts
@@ -6,8 +6,8 @@ export class VideoRoutes {
         static configureRoutes() {
                 const controller = new VideoController();
                 const router = express.Router();
-                router.post("/", controller.getVideos);
-                router.post("/newVideo", authorization(), controller.video);
+                // router.post("/", controller.getVideos);
+                // router.post("/newVideo", authorization(), controller.video);
                 router.post("/answer", authorization(), controller.videoAns);
                 router.post("/getWinnerList",authorization(),  controller.getWinnerList);
                 // router.put("/:vid",authorization(), fileUpload.single('image'), controller.updateVideoById);
